5689daf1b75711e23b205583c7feda7193e57406
AbhinayaKS <abhinaya.kms@gmail.com>
2022-03-22 23:21:16 +0800

    Added Firebase Database

--- Website/src/template/containers/loginAuth.js
+++ Website/src/template/containers/loginAuth.js
@@ -1,13 +1,7 @@
 import { getAuth, createUserWithEmailAndPassword, signInWithEmailAndPassword } from "firebase/auth";
-import { useHistory } from "react-router-dom";
-// Import the functions you need from the SDKs you need
+import { getDatabase, ref, set } from "firebase/database";
 import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
 
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
   apiKey: "AIzaSyCE60wJlGYdIfAUM9wg2SLEKGcXVcbP6JU",
   authDomain: "jugaad-dc4e0.firebaseapp.com",
@@ -15,12 +9,16 @@ const firebaseConfig = {
   storageBucket: "jugaad-dc4e0.appspot.com",
   messagingSenderId: "944531526558",
   appId: "1:944531526558:web:d62865229402e50e8d3c8f",
-  measurementId: "G-2FEN0WZXKX"
+  measurementId: "G-2FEN0WZXKX",
+  databaseURL: "https://jugaad-dc4e0-default-rtdb.asia-southeast1.firebasedatabase.app/"
 };
 
+
+const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
+const database = getDatabase(app);
+
 export function authorizeUser(email, password) {
-    const app = initializeApp(firebaseConfig);
-    const auth = getAuth();
     signInWithEmailAndPassword(auth, email, password)
     .then((userCredential) => {
         // Signed in 
@@ -35,13 +33,11 @@ export function authorizeUser(email, password) {
 }
 
 export function signUpUser(email, password) {
-    // Initialize Firebase
-    const app = initializeApp(firebaseConfig);
-    const auth = getAuth();
     createUserWithEmailAndPassword(auth, email, password)
     .then((userCredential) => {
         // Signed in 
         const user = userCredential.user;
+        writeUserData(user.uid, user.email, "test")
         // ...
     })
     .catch((error) => {
@@ -51,3 +47,19 @@ export function signUpUser(email, password) {
         // ..
     });
 }
+
+export function writeUserData(uid, email, name) {
+    set(ref(database, uid), {
+      email: email,
+    name: name
+    })
+    .then(() => {
+        
+        // ...
+    })
+    .catch((error) => {
+        const errorCode = error.code;
+        const errorMessage = error.message;
+        console.log(errorCode);
+    });
+  }
