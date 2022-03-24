ab359c82907d9d82a7fd30c6ecc8708810d2d828
Aishwarya Singh <aish.akshu@gmail.com>
2022-03-24 12:16:26 +0800

    Add context.js

---
+++ src/store/Context.js
@@ -0,0 +1,21 @@
+import { createContext, useEffect, useState } from "react";
+import { auth } from "../firebase";
+export const FirebaseContext = createContext(null);
+
+export const AuthContext = createContext(null);
+
+export default function Context  ({children})  {
+    const [user, setUser] = useState(null);
+    useEffect(() => {
+        auth.onAuthStateChanged((user) => {
+          setUser(user)
+        });
+      });
+    
+
+    return(
+        <AuthContext.Provider value={{user, setUser}}>
+            {children}
+        </AuthContext.Provider>
+    )
+}
