cf785f99e73612c1e8181605da854eb42abaec92
AbhinayaKS <abhinaya.kms@gmail.com>
2022-03-30 22:15:14 +0800

    added prodPic fetch

--- src/Components/FeatureProduct.js
+++ src/Components/FeatureProduct.js
@@ -1,4 +1,8 @@
+
 import { useHistory } from "react-router-dom";
+import { firebasestorage } from "../firebase";
+import { ref, getDownloadURL } from "firebase/storage";
+import { useState } from "react";
 
 function getSecondPart(str) {
   return str.split('%')[1];
@@ -10,11 +14,22 @@ function getFirstPart(str) {
 
 export default function FeatureProduct(props) {
   const history = useHistory();
+  const [url, setURL] = useState("");
 
   function onClick(uid) {
     history.push("/Homepage" + getFirstPart(uid));
   }
-  
+
+  getDownloadURL(ref(firebasestorage, props.picPath))
+  .then((url) => {
+    console.log(url);
+    setURL(url);
+  })
+  .catch((error) => {
+    // Handle any errors
+    console.log(error.message);
+  });
+
   return (
     <div className="col">
       <div className="card shadow-sm">
@@ -22,7 +37,7 @@ export default function FeatureProduct(props) {
           className="card-img-top bg-dark cover"
           height="240"
           alt=""
-          src={Image}
+          src={url}
           onClick={() => onClick(props.prodName)}
         />
         <div className="card-body">
